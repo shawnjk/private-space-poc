c223db02bec7c4bf0d9fd66ea1d06ed0f42c31f1
Shawn Kim <me@shawnjkim.com>
2020-02-06 11:47:06 -0600

    Add redis

--- app.js
+++ app.js
@@ -2,6 +2,37 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.json());
+
+const redis = require('redis');
+const redisUrl = process.env.REDIS_URL;
+const client = redis.createClient(redisUrl);
+const {promisify} = require('util');
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
+app.post('/', async (req, res) => {
+  const { body } = req;
+  for (key in body) {
+    await setAsync(key, body[key]);
+  }
+  res.sendStatus(200);
+});
+
+app.get('/', async (req, res) => {
+  const { key } = req.query;
+  if (!key) {
+    res.sendStatus(400);
+    return;
+  }
+
+  const value = await getAsync(key);
+  if (!value) {
+    res.sendStatus(404);
+    return;
+  }
+
+  res.json({ [key]: value });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
